fd83426d0b68330443b552ff1e5df32cacee1633
Lucas Torri <lucastorri@gmail.com>
2010-11-10 20:09:10 -0200

    Adding issues.

--- src/ditado.rb
+++ src/ditado.rb
@@ -15,7 +15,8 @@ module Ditado
   
     def initialize(repo_path)
       @repo_path = repo_path
-      @ditado_folder = @repo_path + '/' + REPO_FOLDER_NAME
+      @ditado_folder = "#{@repo_path}/#{REPO_FOLDER_NAME}"
+      @issues_folder = "#{@ditado_folder}/#{ISSUES_FOLDER_NAME}"
     end
   
     def init
@@ -25,6 +26,33 @@ module Ditado
         return true
       rescue
         return false
+      end
+    end
+    
+    def issue_add(msg)
+      new_issue_id = last_issue_id + 1
+      open("#{@issues_folder}/#{new_issue_id}", 'w') do |f|
+        f.write msg
+      end
+      new_issue_id
+    end
+    
+    def issue_get(id)
+      issue_file = "#{@issues_folder}/#{id}"
+      if File.exists? issue_file then
+        open(issue_file) do |f|
+          return f.read
+        end
+      end
+    end
+    
+    private 
+    def last_issue_id
+      issues_folder_entries = Dir.new(@issues_folder).entries
+      if issues_folder_entries == ['.', '..'] then
+        -1
+      else 
+        issues_folder_entries.map { |issue_filename| issue_filename.to_i }.sort.last
       end
     end
   
--- test/ditado_test.rb
+++ test/ditado_test.rb
@@ -9,14 +9,25 @@ DITADO_WIKI_FOLDER = DITADO_FILES_FOLDER + '/wiki'
 DITADO_WIKI_HOME_FILE = DITADO_WIKI_FOLDER + '/index'
 DITADO_ISSUES_FOLDER = DITADO_FILES_FOLDER + '/issues'
 
+def setup_environment
+  begin 
+    FileUtils.mkdir DITADO_TEST_ENVIRONMENT
+  rescue Exception
+  end
+end
+
+def teardown_environment
+  begin
+    FileUtils.rm_rf DITADO_TEST_ENVIRONMENT
+  rescue Exception
+  end
+end
+
 describe Ditado, 'when ditado is initted on a given folder where' do
   
   before(:each) do
     @ditado = Ditado::Ditado.new DITADO_TEST_ENVIRONMENT
-    begin 
-      FileUtils.mkdir DITADO_TEST_ENVIRONMENT
-    rescue Exception
-    end
+    setup_environment
   end
   
   context 'ditado not initted' do
@@ -32,9 +43,9 @@ describe Ditado, 'when ditado is initted on a given folder where' do
       @ditado.init.should be_true
       (File.file? DITADO_PROJECT_DESCRIPTION_FILE).should be_true
       
-      open(DITADO_PROJECT_DESCRIPTION_FILE).read.should == 
-      "Name: \n" +
-      'Description: '
+      open(DITADO_PROJECT_DESCRIPTION_FILE) do |f|
+        f.read.should == "Name: \nDescription: "
+      end
     end
     
     it 'should create a wiki folder' do
@@ -43,7 +54,9 @@ describe Ditado, 'when ditado is initted on a given folder where' do
       @ditado.init.should be_true
       (File.directory?DITADO_WIKI_FOLDER).should be_true
       (File.file? DITADO_WIKI_HOME_FILE).should be_true
-      open(DITADO_WIKI_HOME_FILE).read.should == 'Welcome!'
+      open(DITADO_WIKI_HOME_FILE) do |f|
+        f.read.should == 'Welcome!'
+      end
     end
     
     it 'should create a issues folder' do
@@ -73,10 +86,53 @@ describe Ditado, 'when ditado is initted on a given folder where' do
   end
   
   after(:each) do
-    begin
-      FileUtils.rm_rf DITADO_TEST_ENVIRONMENT
-    rescue Exception
+    teardown_environment
+  end
+  
+end
+
+describe Ditado, 'when working with issues' do
+  
+  ISSUE_MESSAGE_1 = 'This software does not work.'
+  ISSUE_MESSAGE_2 = 'It is still not working, dam you!'
+  
+  before(:each) do
+    setup_environment
+    @ditado = Ditado::Ditado.new DITADO_TEST_ENVIRONMENT
+    @ditado.init
+  end
+  
+  it 'should be able to add and persist new issues' do
+    issue_id_1 = @ditado.issue_add ISSUE_MESSAGE_1
+    issue_id_2 = @ditado.issue_add ISSUE_MESSAGE_2
+    
+    open("#{DITADO_ISSUES_FOLDER}/0") do |f|
+      f.read.should == ISSUE_MESSAGE_1
     end
+    
+    open("#{DITADO_ISSUES_FOLDER}/1") do |f|
+      f.read.should == ISSUE_MESSAGE_2
+    end    
+  end
+  
+  context 'and retrieving issues' do
+  
+    it 'should be able to retrieve existent issues' do
+      issue_id_1 = @ditado.issue_add ISSUE_MESSAGE_1
+      issue_id_2 = @ditado.issue_add ISSUE_MESSAGE_2
+    
+      @ditado.issue_get(issue_id_1).should == ISSUE_MESSAGE_1
+      @ditado.issue_get(issue_id_2).should == ISSUE_MESSAGE_2
+    end
+    
+    it 'should return nil when the issue does not exist' do
+      @ditado.issue_get(51).should be_nil
+    end
+  
+  end
+  
+  after(:each) do
+    teardown_environment
   end
   
 end
