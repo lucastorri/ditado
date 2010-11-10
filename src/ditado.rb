835d9d8af1fc98fd72e6506ea5ff369521c028d0
Lucas Torri <lucastorri@gmail.com>
2010-11-10 19:23:56 -0200

    Ditado init creating initial structure

--- src/ditado.rb
+++ src/ditado.rb
@@ -1,15 +1,29 @@
 require 'FileUtils'
 
+RESOURCES_FOLDER = File.expand_path(File.dirname(__FILE__) + '/../res')
+SKELETON_FOLDER = RESOURCES_FOLDER + '/skeleton'
+
 class Ditado
   
   REPO_FOLDER_NAME = '.ditado'
+  PROJECT_DESC_FILE = 'project'
+  WIKI_FOLDER_NAME = 'wiki'
+  WIKI_HOME_FILE = 'index'
+  ISSUES_FOLDER_NAME = 'issues'
   
   def initialize(repo_path)
     @repo_path = repo_path
+    @ditado_folder = @repo_path + '/' + REPO_FOLDER_NAME
   end
   
   def init
-    FileUtils.mkdir @repo_path + '/' + REPO_FOLDER_NAME
+    begin
+      throw Exception.new('Folder already exists') if File.exists? @ditado_folder
+      FileUtils.cp_r(SKELETON_FOLDER, @ditado_folder)
+      return true
+    rescue
+      return false
+    end
   end
   
 end
--- test/ditado_test.rb
+++ test/ditado_test.rb
@@ -1,27 +1,78 @@
 require 'rspec'
 require 'FileUtils'
-require File.expand_path(File.dirname(__FILE__) + "/../src/ditado.rb")
+require File.expand_path(File.dirname(__FILE__) + '/../src/ditado.rb')
 
-describe Ditado, 'when ditado is initted' do
+describe Ditado, 'when ditado is initted on a given folder where' do
 
-  DITADO_TEST_ENVIRONMENT = File.dirname(__FILE__) + "/run"
+  DITADO_TEST_ENVIRONMENT = File.dirname(__FILE__) + '/run'
   DITADO_FILES_FOLDER = DITADO_TEST_ENVIRONMENT + '/.ditado'
+  DITADO_PROJECT_DESCRIPTION_FILE = DITADO_FILES_FOLDER + '/project'
+  DITADO_WIKI_FOLDER = DITADO_FILES_FOLDER + '/wiki'
+  DITADO_WIKI_HOME_FILE = DITADO_WIKI_FOLDER + '/index'
+  DITADO_ISSUES_FOLDER = DITADO_FILES_FOLDER + '/issues'
   
-  before(:all) do
+  before(:each) do
+    @ditado = Ditado.new DITADO_TEST_ENVIRONMENT
     begin 
       FileUtils.mkdir DITADO_TEST_ENVIRONMENT
     rescue Exception
     end
   end
   
-  it 'should create a .ditado folder on the given repo' do
-    (File.exists? DITADO_FILES_FOLDER).should be_false
-    ditado = Ditado.new DITADO_TEST_ENVIRONMENT
-    ditado.init
-    (File.exists? DITADO_FILES_FOLDER).should be_true
+  context 'ditado not initted' do
+  
+    it 'should create a .ditado folder' do
+      (File.exists? DITADO_FILES_FOLDER).should be_false
+      @ditado.init.should be_true
+      (File.directory?DITADO_FILES_FOLDER).should be_true
+    end
+  
+    it 'should create a project details file' do
+      (File.exists? DITADO_PROJECT_DESCRIPTION_FILE).should be_false
+      @ditado.init.should be_true
+      (File.file? DITADO_PROJECT_DESCRIPTION_FILE).should be_true
+      
+      open(DITADO_PROJECT_DESCRIPTION_FILE).read.should == 
+      "Name: \n" +
+      'Description: '
+    end
+    
+    it 'should create a wiki folder' do
+      (File.exists? DITADO_WIKI_FOLDER).should be_false
+      (File.exists? DITADO_WIKI_HOME_FILE).should be_false
+      @ditado.init.should be_true
+      (File.directory?DITADO_WIKI_FOLDER).should be_true
+      (File.file? DITADO_WIKI_HOME_FILE).should be_true
+      open(DITADO_WIKI_HOME_FILE).read.should == 'Welcome!'
+    end
+    
+    it 'should create a issues folder' do
+      (File.exists? DITADO_ISSUES_FOLDER).should be_false
+      @ditado.init.should be_true
+      (File.directory?DITADO_ISSUES_FOLDER).should be_true
+    end
+  
   end
   
-  after(:all) do
+  context 'ditado already innited' do
+  
+    before(:each) do
+      begin 
+        FileUtils.mkdir DITADO_FILES_FOLDER
+      rescue Exception
+      end
+    end
+  
+    it 'should not modify anything' do
+      (File.exists? DITADO_FILES_FOLDER).should be_true
+      files_before = Dir.new(DITADO_FILES_FOLDER).entries
+      @ditado.init.should be_false
+      files_before.should == Dir.new(DITADO_FILES_FOLDER).entries
+    end
+    
+  end
+  
+  after(:each) do
     begin
       FileUtils.rm_rf DITADO_TEST_ENVIRONMENT
     rescue Exception
